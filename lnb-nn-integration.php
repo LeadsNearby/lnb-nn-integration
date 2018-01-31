5bdbeb43bae4acf3d71c12f61ece0f544ed84deb
Andrew Gillingham <andrew@leadsnearby.com>
2018-01-31 15:23:21 -0500

    Bump version number

--- lnb-nn-integration.php
+++ lnb-nn-integration.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: LeadsNearby Nearby Now Stynamic Integration
 Description: Includes an API class that gets and stores Nearby Now data in the database for easy retrieval. Also includes a widget.
-Version: 1.0.0
+Version: 1.0.1
 Author: LeadsNearby (Andrew Gillingham)
 */
 
