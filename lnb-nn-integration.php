9da2f1a3472f792b65d3c30b7a9c49ad46869b58
DeliciousLayao <mike@leadsnearby.com>
2018-08-13 15:34:20 -0400

    Update lnb-nn-integration.php

--- lnb-nn-integration.php
+++ lnb-nn-integration.php
@@ -9,9 +9,10 @@ Author: LeadsNearby (Andrew Gillingham)
 
 require_once( plugin_dir_path( __FILE__ ) . '/lib/class-nn-api.php' );
 require_once( plugin_dir_path( __FILE__ ) . '/lib/class-nn-widget.php' );
+require_once( plugin_dir_path( __FILE__ ) . '/lib/class-nn-testimonial-widget.php' );
 
 new NN_Static_Widget();
-
+new NN_Testimonial_Widget();
 // add_action( 'save_post', array( 'NN_API', 'reset_cache' ) );
 add_action( 'wp_update_nav_menu', array( 'NN_API', 'reset_cache' ) );
 add_action( 'after_rocket_clean_cache_dir', array( 'NN_API', 'reset_cache' ) );
